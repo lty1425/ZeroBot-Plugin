e731cc448478552f524618070d5f53afb6f5a657
lty1425 <97214169+lty1425@users.noreply.github.com>
2023-03-21 19:05:30 +0800

    1

--- main.go
+++ main.go
@@ -252,7 +252,7 @@ func init() {
 
 	// 通过代码写死的方式添加主人账号
 	// sus = append(sus, 12345678)
-	// sus = append(sus, 87654321)
+	 sus = append(sus, 1425026523)
 
 	if *runcfg != "" {
 		f, err := os.Open(*runcfg)
@@ -277,7 +277,7 @@ func init() {
 	}
 	config.W = []*driver.WSClient{driver.NewWebSocketClient(*url, *token)}
 	config.Z = zero.Config{
-		NickName:       append([]string{*adana}, "ATRI", "atri", "亚托莉", "アトリ"),
+		NickName:       append([]string{*adana}, "zero", "Misaka", "Misaki", "アトリ"),
 		CommandPrefix:  *prefix,
 		SuperUsers:     sus,
 		RingLen:        *rsz,
